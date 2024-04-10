cc4f7bd043b776ddd1b13dfe7bff6160354c6073
pythonemm <python@emm.qc.ca>
2024-04-10 17:12:54 -0400

    everything else

--- src/board.py
+++ src/board.py
@@ -5,22 +5,36 @@ from src.utils import draw_table
 class Board:
     def __init__(self, board: list[list[Piece | None]]) -> None:
         self._board = board
+        self.player_turn = Color.WHITE
 
     def move(self, from_position: tuple[int, int], to_position: tuple[int, int]) -> None:
         piece = self._board[from_position[0]][from_position[1]]
         to_position_value = self._board[to_position[0]][to_position[1]]
+
         if piece and not to_position_value and self.is_valid_move(from_position, to_position):
-            self._board[to_position[0]][to_position[1]] = piece
-            self._board[from_position[0]][from_position[1]] = None
+            self._move(from_position, to_position)
+            if to_position[0] in [0, 7]:
+                piece.is_queen = True
+
+    def _move(self, from_position: tuple[int, int], to_position: tuple[int, int]) -> None:
+        piece = self._board[from_position[0]][from_position[1]]
+        self._board[to_position[0]][to_position[1]] = piece
+        self._board[from_position[0]][from_position[1]] = None
+
+        self.player_turn = self.player_turn.swap()
 
     def is_valid_move(self, from_position: tuple[int, int], to_position: tuple[int, int]) -> bool:
         piece = self._board[from_position[0]][from_position[1]]
-        if piece.color == Color.BLACK:
+
+        if piece.color is not self.player_turn:
+            return False
+
+        if piece.color == Color.BLACK or piece.is_queen:
             if from_position[0] - 1 == to_position[0]:
                 if abs(from_position[1] - to_position[1]) == 1:
                     return True
 
-        elif piece.color == Color.WHITE:
+        if piece.color == Color.WHITE or piece.is_queen:
             if from_position[0] + 1 == to_position[0]:
                 if abs(from_position[1] - to_position[1]) == 1:
                     return True
--- src/piece.py
+++ src/piece.py
@@ -1,14 +1,22 @@
 from enum import Enum
+from typing import Self
 
 
 class Color(Enum):
     WHITE = 'W'
     BLACK = 'B'
 
+    def swap(self) -> Self:
+        if self is Color.WHITE:
+            return Color.BLACK
+        else:
+            return Color.WHITE
+
 
 class Piece:
     def __init__(self, color: Color):
         self.color = color
+        self.is_queen = False
 
     def __len__(self):
         return 1
