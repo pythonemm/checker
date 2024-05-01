63bf75bcf684ae1b3207fde3bd479e10af3152da
pythonemm <python@emm.qc.ca>
2024-05-01 17:11:31 -0400

    last course

--- src/board.py
+++ src/board.py
@@ -11,35 +11,59 @@ class Board:
         piece = self._board[from_position[0]][from_position[1]]
         to_position_value = self._board[to_position[0]][to_position[1]]
 
-        if piece and not to_position_value and self.is_valid_move(from_position, to_position):
-            self._move(from_position, to_position)
+        is_valid_move, as_captured = self.is_valid_move_and_capture(from_position, to_position)
+        if piece and not to_position_value and is_valid_move:
+            self._move(from_position, to_position, as_captured)
             if to_position[0] in [0, 7]:
                 piece.is_queen = True
 
-    def _move(self, from_position: tuple[int, int], to_position: tuple[int, int]) -> None:
+    def _move(self, from_position: tuple[int, int], to_position: tuple[int, int], as_captured: bool) -> None:
         piece = self._board[from_position[0]][from_position[1]]
         self._board[to_position[0]][to_position[1]] = piece
         self._board[from_position[0]][from_position[1]] = None
 
+        if as_captured:
+            capture_piece_col, capture_piece_row = self.get_middle_piece(from_position, to_position)
+            self._board[capture_piece_row][capture_piece_col] = None
         self.player_turn = self.player_turn.swap()
 
-    def is_valid_move(self, from_position: tuple[int, int], to_position: tuple[int, int]) -> bool:
+    def is_valid_move_and_capture(self, from_position: tuple[int, int], to_position: tuple[int, int]) -> (bool, bool):
         piece = self._board[from_position[0]][from_position[1]]
 
+        # si ton tour
         if piece.color is not self.player_turn:
-            return False
+            return False, False
 
         if piece.color == Color.BLACK or piece.is_queen:
             if from_position[0] - 1 == to_position[0]:
                 if abs(from_position[1] - to_position[1]) == 1:
-                    return True
+                    return True, False
+
+            elif from_position[0] - 2 == to_position[0]:
+                if abs(from_position[1] - to_position[1]) == 2:
+                    capture_piece_col, capture_piece_row = self.get_middle_piece(from_position, to_position)
+                    capture_piece = self._board[capture_piece_row][capture_piece_col]
+                    if capture_piece.color is not self.player_turn:
+                        return True, True
 
         if piece.color == Color.WHITE or piece.is_queen:
             if from_position[0] + 1 == to_position[0]:
                 if abs(from_position[1] - to_position[1]) == 1:
-                    return True
+                    return True, False
 
-        return False
+            elif from_position[0] + 2 == to_position[0]:
+                if abs(from_position[1] - to_position[1]) == 2:
+                    capture_piece_col, capture_piece_row = self.get_middle_piece(from_position, to_position)
+                    capture_piece = self._board[capture_piece_row][capture_piece_col]
+                    if capture_piece.color is not self.player_turn:
+                        return True, True
+
+        return False, False
+
+    def get_middle_piece(self, from_position, to_position):
+        capture_piece_row = int((from_position[0] + to_position[0]) / 2)
+        capture_piece_col = int((from_position[1] + to_position[1]) / 2)
+        return capture_piece_col, capture_piece_row
 
     def get_board(self) -> list[list[Piece]]:
         return self._board
